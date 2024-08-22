07b9699a3d7e0b453627ab23a5cbae9ebd572684
Integrator16 <106600860+Integrator16@users.noreply.github.com>
2024-08-22 11:47:30 +0300

    Update Addtasks.ps1

--- Addtasks.ps1
+++ Addtasks.ps1
@@ -3,14 +3,14 @@ $sourceSmartctl = ".\smartctl.exe"
 $sourceClient = ".\client.ps1"
 $sourceRunner = ".\1ssd.bat"
 $destinationFolderS32 = "C:\Windows\System32\"
-$destinationFolder1SSD = "C:\PROGRAM FILES\1SSD\"
+$destinationFolder1SSD = "C:\PROGRAM FILES\1SSD"
 
 Copy-Item -Path $sourceSmartctl -Destination $destinationFolderS32 -Force
 Copy-Item -Path $sourceClient -Destination $destinationFolder1SSD -Force
 Copy-Item -Path $sourceRunner -Destination $destinationFolder1SSD -Force
 
 # Создание задачи в Планировщике заданий
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy RemoteSigned -File `"$destinationFolder1SSD\1ssd.bat`""
+$action = New-ScheduledTaskAction -Execute "$destinationFolder1SSD\1ssd.bat" -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy RemoteSigned
 $trigger = New-ScheduledTaskTrigger -Daily -At 11:59am 
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
