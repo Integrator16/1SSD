e25fb3a65538f2e191ad73de3cc113c285240fbf
Integrator16 <106600860+Integrator16@users.noreply.github.com>
2024-09-17 16:01:16 +0300

    Update Addtasks.ps1

--- Addtasks.ps1
+++ Addtasks.ps1
@@ -10,7 +10,7 @@ Copy-Item -Path $sourceClient -Destination $destinationFolder1SSD -Force
 Copy-Item -Path $sourceRunner -Destination $destinationFolder1SSD -Force
 
 # Создание задачи в Планировщике заданий
-$action = New-ScheduledTaskAction -Execute "$destinationFolder1SSD\1ssd.bat" -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy RemoteSigned
+$action = New-ScheduledTaskAction -Execute "$destinationFolder1SSD\1ssd.bat" -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy RemoteSigned"
 $trigger = New-ScheduledTaskTrigger -Daily -At 11:59am 
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
